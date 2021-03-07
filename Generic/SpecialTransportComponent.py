5c38ca3adcc293ede762183b1853e041b9f30f3d
srsbzns@fastmail.fm <srsbzns@fastmail.fm>
2021-03-06 16:06:44 -0800

    Updated imports for Python 3 (Live 11 change).

--- Generic/SpecialTransportComponent.py
+++ Generic/SpecialTransportComponent.py
@@ -4,8 +4,8 @@ from _Framework.ButtonElement import ButtonElement
 from _Framework.EncoderElement import EncoderElement 
 from _Framework.SubjectSlot import subject_slot 
 
-from MIDI_Map import TEMPO_TOP
-from MIDI_Map import TEMPO_BOTTOM
+from .MIDI_Map import TEMPO_TOP
+from .MIDI_Map import TEMPO_BOTTOM
 class SpecialTransportComponent(TransportComponent):
     __doc__ = ' TransportComponent that only uses certain buttons if a shift button is pressed '
     def __init__(self):
